f12d88003d82db6aaee9e27746bdf1c14da4a362
devolart <125961492+devolart@users.noreply.github.com>
2024-06-30 16:01:58 +0700

    Create noroot.py

---
+++ noroot.py
@@ -0,0 +1,75 @@
+import subprocess, time, json, shutil, os
+from threading import Thread
+
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+USER_ID = int(os.getenv("USER_ID", 0))
+DUMP_ID = int(os.getenv("DUMP_ID", 0))
+
+Working = True
+
+banner = '''
+
+ ____   ____.______  ._______  .______       _____._.______  .___  ____   ____
+ \\   \\_/   /: __   \\ : .___  \\ :      \\      \\__ _:|: __   \\ : __| \\   \\_/   /
+  \\___ ___/ |  \\____|| :   |  ||       |       |  :||  \\____|| : |  \\___ ___/ 
+  /   _   \\ |   :  \\ |     :  ||   |   |       |   ||   :  \\ |   |  /   _   \\ 
+ /___/ \\___\\|   |___\\ \\_. ___/ |___|   |       |   ||   |___\\|   | /___/ \\___\\
+            |___|       :/         |___|       |___||___|    |___|            
+                        :                                                     
+                                                                              
+ 
+              _____     __     __     __              __          
+             / ___/__  / /__ _/ /    / / ___ ___ ____/ /  ___ ____
+            / /__/ _ \\/ / _ `/ _ \\  / /_/ -_) -_) __/ _ \\/ -_) __/
+            \\___/\\___/_/\\_,_/_.__/ /____|__/\\__/\\__/_//_/\\__/_/   
+
+                                                
+
+'''
+
+print(banner)
+
+def Loading():
+    white = 37
+    black = 0
+    while Working:
+        print("\r" + "░"*white + "▒▒"+ "▓"*black + "▒▒" + "░"*white, end="")
+        black = (black + 2) % 75
+        white = (white -1) if white != 0 else 37
+        time.sleep(2)
+    clear_output()
+
+
+_Thread = Thread(target=Loading, name="Prepare", args=())
+_Thread.start()
+
+if len(str(DUMP_ID)) == 10 and "-100" not in str(DUMP_ID):
+    n_dump = "-100" + str(DUMP_ID)
+    DUMP_ID = int(n_dump)
+
+cmd = "git clone https://github.com/XronTrix10/Telegram-Leecher"
+proc = subprocess.run(cmd, shell=True)
+cmd = "pip3 install -r Telegram-Leecher/requirements.txt"
+proc = subprocess.run(cmd, shell=True)
+
+credentials = {
+    "API_ID": API_ID,
+    "API_HASH": API_HASH,
+    "BOT_TOKEN": BOT_TOKEN,
+    "USER_ID": USER_ID,
+    "DUMP_ID": DUMP_ID,
+}
+
+with open('Telegram-Leecher/credentials.json', 'w') as file:
+    file.write(json.dumps(credentials))
+
+Working = False
+
+if os.path.exists("Telegram-Leecher/my_bot.session"):
+    os.remove("Telegram-Leecher/my_bot.session") # Remove previous bot session
+    
+print("\rStarting Bot....")
+
+! cd Telegram-Leecher/ && python3 -m colab_leecher
